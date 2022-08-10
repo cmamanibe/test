6fb05a5469a6bb8bc28dc5b7be181e61fe1882ec
cesar <cesar.mamani@est.fi.uncoma.edu.ar>
2022-08-10 19:37:38 -0300

    cambio a ejericio 1 de repaso POO

---
+++ instrumento.java
@@ -0,0 +1,29 @@
+import java.util.*
+public class instrumento {
+    public void tocar() {
+        System.out.println("Instrumento.tocar()"");
+    }
+    public String tipo(){
+        return "iInstrumento";
+    }
+
+    public void afinar(){
+
+    }
+}
+
+class Guitarra extends Instrumento{
+    public void tocar(){
+        system.out.println("Guitarra.tocar()")
+    }
+    public String tipo(){ return "Guitarra";}
+    public void afinar(){}
+}
+class Piano extends Instrumento{
+    public void tocar(){
+        system.out.println("Piano.tocar()");}
+        
+        public string tipo(){return "Pano";}
+        public void afinar(){}
+    }
+}
