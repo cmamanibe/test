c036534627a9a24cd5396f255fde25f9c0f06197
cesar <cesar.mamani@est.fi.uncoma.edu.ar>
2022-08-10 20:09:20 -0300

    class main

--- instrumento.java
+++ instrumento.java
@@ -51,4 +51,26 @@ public void afinar(){
                 return "Ukelele";
             }
                 }
-                
+
+public class Musica{
+    //No importa el tipo d instrumento,
+    // seguira funcionando debido a polimorfismo:
+    static void afinar(Instrumento i){
+        // ...
+        i.tocar();
+    }
+    static void afinarTodo( Instrumento[] e){
+        for (int i=0;i<e.length;i++)
+        afinar(e[i]);
+    }
+    public static void main(String[] args){
+        Instrumento[] orquesta=new Instrumento[5];
+        int i=0;
+        //Up-casting al asignarse el Arreglo
+        orquesta[i++]=new Guitarra();
+        orquesta[i++]=new Piano();
+        orquesta[i++]=new Saxofon();
+        orquesta[i++]=new Guzla();
+        orquesta[i++]=new Ukelele();
+    }
+}//clase musica
