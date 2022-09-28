1dc5b5c9d313f7cfa4affb169e18962b01a742f1
cmamani <cesar.mamani@est.fi.uncoma.edu.ar>
2022-09-28 18:33:08 -0300

    se añadio funcionalidad a los barcos

--- Puerto.java
+++ Puerto.java
@@ -7,10 +7,27 @@ class barco{
     public String Matricula;
     public int eslora;
     public int Año_Fabricacion;
+    public int dni_propietario;
+    public barco(String patente, int largo, int fabricacion, int propietario){
+        Matricula=patente;
+        eslora=largo;
+        Año_Fabricacion=fabricacion;
+        dni_propietario=propietario;
+    }
 }
 
 class velero extends barco{
     public int nro_mastil;
+    public barco(String patente, int largo, int fabricacion, int propietario, int cantidad_mastil){
+        Matricula=patente;
+        eslora=largo;
+        Año_Fabricacion=fabricacion;
+        dni_propietario=propietario;
+        nro_mastil=cantidad_mastil;
+    }
+    public int getMastil(){
+        return nro_mastil;
+    }
 }
 
 class deportivos extends barco{
@@ -27,9 +44,15 @@ class alquiler{
     public Date f_final=new Date();
     public int nro_amarre;
     public  long valor_Alquiler;
-    public void calcularAlquiler(int largo){
-        valor_Alquiler=(f_final.getDay()-f_inicial.getDay())*10*largo;
+    public alquiler(Date fecha_inicial, Date fecha_final, int amarre){
+        f_inicial=fecha_inicial;
+        f_final=fecha_final;
+        nro_amarre=amarre;
     }
+    public void calcularAlquiler(int largo){
+        valor_Alquiler=(f_final.getTime()-f_inicial.getTime())*10*largo;
+    }
+
 }
 public class Puerto {
     
