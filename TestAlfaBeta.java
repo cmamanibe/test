10953b2c9b645f30659b7c15e9e6570dd849cae2
cesar <cesar.mamani@est.fi.uncoma.edu.ar>
2023-08-16 19:52:42 -0300

    agregue funcion clrscr

--- TestAlfaBeta.java
+++ TestAlfaBeta.java
@@ -1,5 +1,8 @@
 public class TestAlfaBeta{
     public static void main(String[] args){
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+
         HiloAlfaBeta alfa= new HiloAlfaBeta("Hilo Alfa", 3);
         HiloAlfaBeta beta= new HiloAlfaBeta("Hilo Beta", 4);
 
