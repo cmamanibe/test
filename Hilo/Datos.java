026869f06d3ca89b38fd9c574f6a94f331b3b945
cesar <cesar.mamani@est.fi.uncoma.edu.ar>
2023-08-23 19:43:39 -0300

    agrego test mihilo

---
+++ Hilo/Datos.java
@@ -0,0 +1,17 @@
+package Hilo;
+
+public class Datos {
+    private int dato=0;
+    public Datos(int nro){
+    dato = nro;
+ }
+    public int getDato(){
+    return dato;
+ }
+    public void setDato(int valor){
+    dato = valor;
+ }
+    public boolean verificar(int valor){
+    return dato>valor;
+ }
+}
